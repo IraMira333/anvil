c4874a26d4d9eb23c40cbc798c368fd7f329e75c
IraMira333 <irinat333@gmail.com>
2025-05-17 22:39:19 +0300

    Test husky

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,55 @@
+import { FlatCompat } from "@eslint/eslintrc";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
   baseDirectory: __dirname,
+  recommendedConfig: "eslint-config-eslint",
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    ignores: ["node_modules", "build", ".next", "out", "public"],
+  },
+  ...compat.config({
+    plugins: ["tailwindcss", "unused-imports", "simple-import-sort"],
+    extends: ["next/core-web-vitals", "next/typescript", "prettier"],
+    rules: {
+      semi: ["error", "always"],
+      quotes: ["error", "double"],
+      "prefer-arrow-callback": ["error"],
+      "prefer-template": ["error"],
+      "no-unused-vars": [
+        "error",
+        {
+          argsIgnorePattern: "^_",
+          varsIgnorePattern: "^_",
+        },
+      ],
+      "no-console": [
+        "warn",
+        {
+          allow: ["warn", "error"],
+        },
+      ],
+      "unused-imports/no-unused-imports": "error",
+      "unused-imports/no-unused-vars": [
+        "warn",
+        {
+          vars: "all",
+          varsIgnorePattern: "^_",
+          args: "after-used",
+          argsIgnorePattern: "^_",
+        },
+      ],
+      "tailwindcss/no-custom-classname": "warn",
+      "simple-import-sort/imports": "error",
+      "simple-import-sort/exports": "error",
+    },
+  }),
 ];
 
 export default eslintConfig;
---
+++ tailwind.config.ts
@@ -0,0 +1,24 @@
+import forms from "@tailwindcss/forms";
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+    content: [
+        "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+        "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+        "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    ],
+    theme: {
+        screens: {
+            tab: "768px",
+            pc: "1440px",
+        },
+        extend: {
+            fontFamily: {},
+            colors: {},
+            dropShadow: {},
+            backgroundImage: {},
+        },
+    },
+    plugins: [forms],
+};
+export default config;
