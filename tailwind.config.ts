c7859f75a10b311c7c223157fd84641324e957a5
IraMira333 <irinat333@gmail.com>
2025-05-17 23:05:48 +0300

    Updated globals, page, layout and added fonts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,23 +2,26 @@ import forms from "@tailwindcss/forms";
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-    content: [
-        "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-        "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-        "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-    ],
-    theme: {
-        screens: {
-            tab: "768px",
-            pc: "1440px",
-        },
-        extend: {
-            fontFamily: {},
-            colors: {},
-            dropShadow: {},
-            backgroundImage: {},
-        },
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    screens: {
+      tab: "768px",
+      pc: "1440px",
     },
-    plugins: [forms],
+    extend: {
+      fontFamily: {
+        exo: ["var(--font-exo)"],
+        robotoMono: ["var(--font-roboto-mono)"],
+      },
+      colors: {},
+      dropShadow: {},
+      backgroundImage: {},
+    },
+  },
+  plugins: [forms],
 };
 export default config;
