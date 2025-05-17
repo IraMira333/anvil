55b5617f800ef981c4c0e13dfc3999b4bd874b60
IraMira333 <irinat333@gmail.com>
2025-05-18 00:18:32 +0300

    Setuped tailwind

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
   const t = useTranslations("HomePage");
   return (
     <div className="">
-      <h1 className="border border-green-400 ">{t("about")}</h1>
+      <h1 className="">{t("about")}</h1>
       <LanguageSwitcher />
     </div>
   );
