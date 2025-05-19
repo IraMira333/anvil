45ae8e88e0ac34946726d62ce515d5c74daa5196
IraMira333 <irinat333@gmail.com>
2025-05-19 17:40:03 +0300

    Created Icons

---
+++ src/components/Icons/IconUp.tsx
@@ -0,0 +1,18 @@
+import { IconProps } from "@/types/iconProps";
+
+export const IconUp = ({ className }: IconProps) => {
+  return (
+    <svg
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      className={className}
+      aria-label="Up to top the page icon"
+    >
+      <path d="M5 11L11 5.5H13L19 11V13.5L13 8H11L5 13.5V11Z" fill="#CFCFCF" />
+      <path d="M13 8H11V19H13V8Z" fill="#CFCFCF" />
+    </svg>
+  );
+};
