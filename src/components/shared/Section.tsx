db1697abf3419f8538bd1fe4d33d3f726d465970
IraMira333 <irinat333@gmail.com>
2025-05-21 01:06:51 +0300

    Update Section.tsx

--- src/components/shared/Section.tsx
+++ src/components/shared/Section.tsx
@@ -2,15 +2,18 @@ export const Section = ({
   children,
   className,
   id,
+  style,
 }: {
   children: React.ReactNode;
   className?: string;
   id?: string;
+  style?: React.CSSProperties;
 }) => {
   return (
     <section
       id={id}
       className={`px-4 tab:px-5 pc:px-60 max-w-[540px] tab:max-w-full pc:max-w-[1440px] mx-auto ${className}`}
+      style={style}
     >
       {children}
     </section>
