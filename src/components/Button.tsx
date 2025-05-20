8066c609cd9d1046f3a7a7e41961728b21356b85
IraMira333 <irinat333@gmail.com>
2025-05-21 01:06:42 +0300

    Update Button.tsx

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,4 +1,9 @@
-export const Button = ({ text }: { text: string }) => {
+import { useTranslations } from "next-intl";
+
+export const Button = ({ joinUs }: { joinUs?: boolean }) => {
+  const t = useTranslations("Buttons");
+  const text = joinUs ? t("joinUs") : t("callUs");
+
   return (
     <div className="relative w-[280px] h-[56px]">
       <button className=" w-[280px] h-[56px] bg-radial-green-50 uppercase text-accent font-bold text-base hover:bg-radial-green-100 transition-all duration-300 ease-in-out">
