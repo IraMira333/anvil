bfd996c9f9226bf527fbd44ebcc396aaeae75817
IraMira333 <irinat333@gmail.com>
2025-05-21 01:07:32 +0300

    Create Union.tsx

---
+++ src/components/Icons/Union.tsx
@@ -0,0 +1,28 @@
+import { IconProps } from "@/types/iconProps";
+
+export const Union = ({ className }: IconProps) => {
+  return (
+    <svg
+      width="344"
+      height="49"
+      viewBox="0 0 344 49"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      className={className}
+      aria-label="borders icon"
+    >
+      <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M21.7639 0.619457L323.587 0.619457L323.751 0.820591L342.633 24.2295L323.587 47.8396L21.7639 47.8396L1.17828 24.2295L21.7639 0.619457ZM342.633 24.2295L323.587 0.619457L323.751 0.820591L342.633 24.2295Z"
+        fill="#141414"
+      />
+      <path
+        d="M323.587 0.619459L21.7639 0.619459L1.17829 24.2295L21.7639 47.8396L323.587 47.8396L342.633 24.2295M323.587 0.619459L323.751 0.820592L342.633 24.2295M323.587 0.619459L342.633 24.2295"
+        stroke="#CCCCCC"
+        strokeOpacity="0.2"
+        strokeMiterlimit="10"
+      />
+    </svg>
+  );
+};
