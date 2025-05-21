2665fdd91bc6c2fd814933477e03d9dde5766b76
IraMira333 <irinat333@gmail.com>
2025-05-21 21:59:04 +0300

    Added bg-svg

---
+++ src/components/Icons/IconPlay.tsx
@@ -0,0 +1,23 @@
+import { IconProps } from "@/types/iconProps";
+
+export const IconPlay = ({ className }: IconProps) => {
+  return (
+    <svg
+      width="8"
+      height="9"
+      viewBox="0 0 8 9"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      className={className}
+      aria-label="list-style-decorative icon play"
+    >
+      <path
+        d="M1.10547 0.939453L7.20148 4.45945L1.10547 7.97945V0.939453Z"
+        stroke="#13D12F"
+        strokeWidth="0.5"
+        strokeMiterlimit="10"
+        strokeLinecap="round"
+      />
+    </svg>
+  );
+};
