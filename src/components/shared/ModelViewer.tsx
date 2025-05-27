5b9ea564a804b02cc54772606915b6d90406af03
IraMira333 <irinat333@gmail.com>
2025-05-27 17:43:16 +0300

    Added model

---
+++ src/components/shared/ModelViewer.tsx
@@ -0,0 +1,57 @@
+"use client";
+import { Environment, useGLTF } from "@react-three/drei";
+import { Canvas, useFrame } from "@react-three/fiber";
+import { Suspense, useEffect, useRef } from "react";
+import * as THREE from "three";
+
+function Model() {
+  const { scene, animations } = useGLTF("/model/DreoneFPV_TEX.gltf");
+  const group = useRef<THREE.Group>(null);
+  const mixer = useRef<THREE.AnimationMixer | null>(null);
+
+  useEffect(() => {
+    if (animations && animations.length > 0 && group.current) {
+      mixer.current = new THREE.AnimationMixer(group.current);
+      animations.forEach(clip => {
+        mixer.current?.clipAction(clip).play();
+      });
+    }
+  }, [animations]);
+
+  useFrame((state, delta) => {
+    mixer.current?.update(delta);
+  });
+
+  return (
+    <group ref={group} scale={[25, 25, 25]} position={[0, 0, 0]}>
+      <primitive object={scene} />
+    </group>
+  );
+}
+
+export default function ModelViewer() {
+  return (
+    <div className="relative w-full aspect-[288/170] z-10">
+      <Canvas
+        shadows
+        camera={{ position: [0, 0, 10], fov: 50 }}
+        className="absolute inset-0 w-full h-full"
+      >
+        <fog attach="fog" args={["#f5f5f5", 10, 30]} />
+        <ambientLight intensity={1.2} />
+        <spotLight
+          position={[10, 10, 10]}
+          angle={0.3}
+          intensity={2}
+          penumbra={1}
+          castShadow
+        />
+        <hemisphereLight args={["#eeeeee", "#444444", 1]} />
+        <Environment preset="sunset" environmentIntensity={0.3} />
+        <Suspense fallback={null}>
+          <Model />
+        </Suspense>
+      </Canvas>
+    </div>
+  );
+}
