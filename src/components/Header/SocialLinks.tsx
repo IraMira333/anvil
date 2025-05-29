50eb5657c2e115bf25949b0cd653a41434a1d242
IraMira333 <irinat333@gmail.com>
2025-05-29 22:50:13 +0300

    Update SocialLinks.tsx

--- src/components/Header/SocialLinks.tsx
+++ src/components/Header/SocialLinks.tsx
@@ -5,7 +5,7 @@ import ScrambleText from "../shared/ScrambleText";
 export const SocialLinks = ({ className }: { className?: string }) => {
   const whatsapp = "https://wa.me/380955076901";
   const LDLink = "https://www.linkedin.com/";
-  const telegram = "https://www.instagram.com/";
+  const telegram = "https://t.me/anvil_drone";
 
   const locale = useLocale();
 
